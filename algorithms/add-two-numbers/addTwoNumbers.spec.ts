6bf29c3c713b96069de7a9b30db7c10954504a97
caoyong <caoyong@bytedance.com>
2019-04-12 14:30:43 +0800

    add two numbers
    Change-Id: Ibe8554f36306bbf44654697dc9a0d1181d208182

---
+++ algorithms/add-two-numbers/addTwoNumbers.spec.ts
@@ -0,0 +1,16 @@
+import { ListNode, addTwoNumbers } from "./addTwoNumbers";
+
+describe('add two numbers', function () {
+  test('[2, 4, 3], [5, 6, 4]', function () {
+    const l1 = ListNode.from([2, 4, 3]);
+    const l2 = ListNode.from([5, 6, 4]);
+    expect(Array.from(addTwoNumbers(l1, l2))).toEqual([7, 0, 8]);
+  });
+
+  test('[0], [0]', function () {
+    const l1 = ListNode.from([0]);
+    const l2 = ListNode.from([0]);
+    const list = addTwoNumbers(l1, l2)
+    expect(Array.from(list)).toEqual([0]);
+  })
+})
---
+++ algorithms/add-two-numbers/addTwoNumbers.ts
@@ -0,0 +1,57 @@
+export class ListNode {
+  constructor(
+    public val: number,
+    public next: ListNode | null = null
+  ) { }
+
+  public *[Symbol.iterator](): IterableIterator<number> {
+    let head = new ListNode(-1, this);
+    while (head.next !== null && head.next.val >= 0) {
+      yield head.next.val;
+      head.next = head.next.next;
+    }
+  }
+
+  public static from(iteration: {
+    [Symbol.iterator](): IterableIterator<number>
+  }): ListNode {
+    let head = new ListNode(-1, null);
+    let pointer: ListNode = head;
+
+    for (let val of iteration) {
+      pointer.next = new ListNode(val, null);
+      pointer = pointer.next;
+    }
+
+    return head.next!;
+  }
+}
+
+export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode {
+  let l1Val: number;
+  let l2Val: number;
+  let head: ListNode = new ListNode(-1, null);
+  let pointer = head;
+  let carry: 0 | 1 = 0;
+
+  while (l1 && l1.val >= 0 || l2 && l2.val >= 0 || carry > 0) {
+    l1Val = l1 && l1.val || 0;
+    l2Val = l2 && l2.val || 0;
+
+    let sum = l1Val + l2Val + carry;
+    carry = 0;
+
+    if (sum >= 10) {
+      sum -= 10;
+      carry = 1;
+    }
+
+    pointer.next = new ListNode(sum, null);
+    pointer = pointer.next;
+
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
+  }
+
+  return head.next!;
+}
