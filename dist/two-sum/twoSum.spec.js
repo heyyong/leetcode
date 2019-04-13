8de7e4ff76c57626b0c56f7d85b0975729bb5bb2
edencaox <edencaox@gmail.com>
2019-04-14 02:53:29 +0800

    rm dist

--- dist/two-sum/twoSum.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const twoSum_1 = require("./twoSum");
-test("two sums", function () {
-    const nums = [2, 7, 11, 15];
-    const target = 9;
-    expect(twoSum_1.twoSum(nums, target)).toEqual([0, 1]);
-});
