ea6420199fa22228acb66b1ac765ba858482d7a2
Cao Yong <edencaox@gmail.com>
2019-05-09 22:14:52 +0800

    chore:

--- algorithms/three-sum/three-sum.spec.ts
+++ algorithms/three-sum/three-sum.spec.ts
@@ -1,8 +1,12 @@
 import { noOrderedDeepEqual } from '@helper/no-ordered-equal';;
 import { threeSum } from "./three-sum";
 
+const methods = [threeSum];
+
 describe("three sums", function() {
-  test("[-1, 0, 1, 2, -1, -4]", function() {
-    expect(noOrderedDeepEqual(threeSum([-1, 0, 1, 2, -1, -4]), [[-1, 0, 1], [-1, -1, 2]])).toEqual(true);
-  });
+  methods.forEach((threeSum) => {
+    test("[-1, 0, 1, 2, -1, -4]", function() {
+      expect(noOrderedDeepEqual(threeSum([-1, 0, 1, 2, -1, -4]), [[-1, 0, 1], [-1, -1, 2]])).toEqual(true);
+    });
+  })
 });
