15c85cff239d9e57e1123485058edca1732cfa5f
Cao Yong <edencaox@gmail.com>
2019-05-10 17:53:03 +0800

    style: lint

--- algorithms/three-sum-closest/threeSumClosest.ts
+++ algorithms/three-sum-closest/threeSumClosest.ts
@@ -1,23 +1,25 @@
-export function threeSumClosest(nums: number[], target: number):number | null {
-  if (nums.length < 3) throw new Error('need array at least has three number')
-  let n = nums.length;
+export function threeSumClosest(nums: number[], target: number): number | null {
+  if (nums.length < 3) throw new Error("need array at least has three number");
+  const n = nums.length;
   nums = nums.sort((a, b) => a - b);
   let closest: number = nums[0] + nums[1] + nums[2];
-  for (let i = 0; i < n - 2; i ++) {
-    let a = nums[i];
-    let start = i + 1, end = n -1;
-    while(start < end) {
-      let b = nums[start], c = nums[end];
-      let sum = a + b + c;
+  for (let i = 0; i < n - 2; i++) {
+    const a = nums[i];
+    let start = i + 1;
+    let end = n - 1;
+    while (start < end) {
+      const b = nums[start];
+      const c = nums[end];
+      const sum = a + b + c;
 
-      if(Math.abs(sum - target) < Math.abs(closest - target)) {
+      if (Math.abs(sum - target) < Math.abs(closest - target)) {
         closest = sum;
       }
 
-      if(sum > target) {
-        end --;
+      if (sum > target) {
+        end--;
       } else if (sum < target) {
-        start ++;
+        start++;
       } else {
         return target;
       }
--- algorithms/three-sum/three-sum.ts
+++ algorithms/three-sum/three-sum.ts
@@ -35,5 +35,3 @@ export function threeSum(nums: number[]): number[][] {
 
   return results;
 }
-
-
